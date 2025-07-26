4c61c5177da2c6f2ad31057544c7bace58067073
William Santana <euwilliamsantana@gmail.com>
2025-07-26 09:57:02 -0300

    LoadingSearch Page

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,5 +1,25 @@
+"use client"
+import { Skeleton } from "@/components/sckeleton";
+import { useSearchParams } from "next/navigation";
+
 export default function LoadingSearch() {
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get("q")
+
+
   return (
-    <h1>Carregando...</h1>
+    <div className="flex flex-col gap-4 ">
+      <p className="text-sm">Resultado para: <span className="font-semibold">{query}</span></p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+      </div>
+    </div>
   );
 }
