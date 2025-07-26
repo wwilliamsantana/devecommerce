426713ea39e40c1aa08b4b3aca98f55e9598139d
William Santana <euwilliamsantana@gmail.com>
2025-07-26 08:40:36 -0300

    feat: API - Search params

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,22 @@
+import z from "zod"
+import data from "../data.json"
+import { NextRequest } from "next/server"
+
+export async function GET(request: NextRequest) {
+  await new Promise(resovle => setTimeout(resovle, 1000))
+
+  const { searchParams } = request.nextUrl
+
+  const query = z.string().parse(searchParams.get("q"))
+
+  const product = data.products.filter(item => {
+    return item.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  })
+
+  if (!product) {
+    return Response.json({ message: "Product not found" }, { status: 400 })
+  }
+
+
+  return Response.json(product)
+}
